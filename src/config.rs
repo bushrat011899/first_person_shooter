53abae324aafc103ce6c8c5ec00f5c0d471cddf4
Zac Harrold <zac@harrold.com.au>
2023-03-21 15:46:39 +1100

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,45 @@
+use std::io::BufWriter;
 use std::net::SocketAddr;
 use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-pub fn try_load_config() -> Result<Config, ()> {
-    let file = File::open("Settings.toml").map_err(|_| ())?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents).map_err(|_| ())?;
-    
-    let config = toml::from_str(contents.as_str()).map_err(|_| ())?;
-
-    Ok(config)
-}
-
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub matchmaking_server: SocketAddr,
-    pub controls: ControlBindings
+    pub controls: ControlBindings,
+    pub graphics: GraphicsSettings
+}
+
+impl Config {
+    const FILE_NAME: &str = "settings.toml";
+
+    pub fn try_load() -> Result<Self, ()> {
+        let file = File::open(Self::FILE_NAME).map_err(|_| ())?;
+    
+        let mut buf_reader = BufReader::new(file);
+        let mut contents = String::new();
+    
+        buf_reader.read_to_string(&mut contents).map_err(|_| ())?;
+        
+        let config = toml::from_str(contents.as_str()).map_err(|_| ())?;
+    
+        Ok(config)
+    }
+
+    pub fn try_save(&self) -> Result<(), ()> {
+        let contents = toml::to_string(self).map_err(|_| ())?;
+    
+        let file = File::create(Self::FILE_NAME).map_err(|_| ())?;
+    
+        let mut buf_writer = BufWriter::new(file);
+    
+        buf_writer.write(contents.as_bytes()).map_err(|_| ())?;
+        buf_writer.flush().map_err(|_| ())?;
+    
+        Ok(())
+    }
 }
 
 #[derive(Serialize, Deserialize)]
